9bccbfbad745e7731b491b093597b949e41fce1a
Gabriel Sousa <ghostamino08@gmail.com>
2026-03-13 13:23:36 -0300

    Data cleaning notebook and processing data module creation

---
+++ src/data/processing_data.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def processing_data(df: pd.DataFrame) -> pd.DataFrame:
+    print("Processando a limpeza dos dados...")
+    df.dropna(subset= ['texto_review'], inplace=True)
+    print(df.info())
+    return df
