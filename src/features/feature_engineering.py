6cc0b35969c6b7198ea7d7271372a45f51f07eb1
Gabriel Sousa <ghostamino08@gmail.com>
2026-03-28 20:09:00 -0300

    model training

--- src/features/feature_engineering.py
+++ src/features/feature_engineering.py
@@ -2,6 +2,7 @@ import pandas as pd
 from src.features.clean_text import limpeza_texto
 
 def feature_engineering(clean_df: pd.DataFrame) -> pd.DataFrame:
+    print("Aplicando features...")
     clean_df['texto_limpo'] = clean_df['texto_review'].apply(limpeza_texto)
 
     clean_df['sentimento_value'] = clean_df['sentimento'].map({'positivo': 1, 'negativo': 0})
